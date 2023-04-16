6625c1b94a416d9ddc6e1d0dc6c89316e51bb954
David Attlee <pseudogrammer@gmail.com>
2023-04-16 18:21:23 -0400

    add comments to packer.lua

--- lua/pseudogram/packer.lua
+++ lua/pseudogram/packer.lua
@@ -1,12 +1,10 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
 vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
 
+	-- Find things as you need them
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.1',
 		-- or                            , branch = '0.1.x',
@@ -14,6 +12,7 @@ return require('packer').startup(function(use)
 	}
 
 
+	-- Theeemes
 	use({
 		'rose-pine/neovim',
 		as = 'rose-pine',
@@ -22,9 +21,20 @@ return require('packer').startup(function(use)
 		end
 	})
 
+	-- treesiter builds an AST and improves highlighting
 	use({ 'nvim-treesitter/nvim-treesitter', {run= ':TSUpdate'}})
+	-- treesitter playground allows you to look at the AST
 	use({ 'nvim-treesitter/playground'}) 
-	use({ 'ThePrimeagen/harpoon'}) 
+	
+	-- move between files fast
+	use({ 'ThePrimeagen/harpoon'})
+
+	-- see your entire branching undo tree. literal magic
 	use({ 'mbbill/undotree' })
+
+	-- Git on steroids
 	use({ 'tpope/vim-fugitive' })
+
+
+
 end)
