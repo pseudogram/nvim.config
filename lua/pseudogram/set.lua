bea4201f575168c4ca20cfc8ad491fb5a1cced3d
David Attlee <pseudogrammer@gmail.com>
2023-04-16 23:18:23 -0400

    sane sets for vim

---
+++ lua/pseudogram/set.lua
@@ -0,0 +1,39 @@
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+-- open new files from netrw in same window (default)
+vim.g.netrw_browse_split = 0
+
+-- when opening a file in split mode from netrw, file take 75% of screen
+vim.g.netrw_winsize = 75
+
+-- hide netrw banner that gives usage info
+-- vim.g.netrw_banner = 0
