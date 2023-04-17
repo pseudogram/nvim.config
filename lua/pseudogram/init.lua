d7b7be307f7f344d1bac1d3c32ed761eff34711c
David Attlee <pseudogrammer@gmail.com>
2023-04-16 23:17:40 -0400

    some nice replacements for builtins

--- lua/pseudogram/init.lua
+++ lua/pseudogram/init.lua
@@ -1,1 +1,38 @@
 require("pseudogram.remap")
+-- require("pseudogram.packer")
+require("pseudogram.set")
+
+
+local augroup = vim.api.nvim_create_augroup
+local pseudogramGroup = augroup('pseudogram', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+-- a lua function that I also don't know how to use
+--
+-- function R(name)
+--     require("plenary.reload").reload_module(name)
+-- end
+-- ]]
+
+-- briefly highlight text when you yank
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+
+-- when saving delete all trailing whitespace
+autocmd({"BufWritePre"}, {
+    group = pseudogramGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
