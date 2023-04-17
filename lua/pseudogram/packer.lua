cab228f2f7065878895ad5c330a9eebd0a80896f
David Attlee <pseudogrammer@gmail.com>
2023-04-17 00:38:27 -0400

    add some more great plugins to autocomplete things

--- lua/pseudogram/packer.lua
+++ lua/pseudogram/packer.lua
@@ -4,65 +4,81 @@
 vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	-- Find things as you need them
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    -- Find things as you need them
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
 
-	-- Theeemes
-	use({
-		'rose-pine/neovim',
-		as = 'rose-pine',
-		config = function()
-			require('rose-pine').setup({
-				variant = 'moon',
-				disable_background = true,
+    -- Theeemes
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            require('rose-pine').setup({
+                variant = 'moon',
+                disable_background = true,
 
-			})
-			vim.cmd('colorscheme rose-pine')
-		end
-	})
+            })
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
 
-	-- treesiter builds an AST and improves highlighting
-	use({ 'nvim-treesitter/nvim-treesitter', {run= ':TSUpdate'}})
-	-- treesitter playground allows you to look at the AST
-	use({ 'nvim-treesitter/playground'})
+    -- treesiter builds an AST and improves highlighting
+    use({
+        'nvim-treesitter/nvim-treesitter',
+        {run= ':TSUpdate'},
+    })
 
-	-- move between files fast
-	use({ 'ThePrimeagen/harpoon'})
 
-	-- see your entire branching undo tree. literal magic
-	use({ 'mbbill/undotree' })
+    -- treesitter playground allows you to look at the AST
+    use({ 'nvim-treesitter/playground'})
 
-	-- Git on steroids
-	use({ 'tpope/vim-fugitive' })
-	use({ 'tpope/vim-commentary' })
+    -- move between files fast
+    use({ 'ThePrimeagen/harpoon'})
 
-	-- LSP
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{                                      -- Optional
-			'williamboman/mason.nvim',
-			run = function()
-				pcall(vim.cmd, 'MasonUpdate')
-			end,
-		},
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+    -- see your entire branching undo tree. literal magic
+    use({ 'mbbill/undotree' })
 
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},     -- Required
-		{'hrsh7th/cmp-nvim-lsp'}, -- Required
-		{'L3MON4D3/LuaSnip'},     -- Required
-	}
-}
+    -- Git on steroids
+    use({ 'tpope/vim-fugitive' })
+    use({ 'tpope/vim-commentary' })
+
+    -- LSP
+    use({
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    })
+    -- add closing brakets automatically
+    use({
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    })
+    -- closing tags for xml like text
+    use({
+        'windwp/nvim-ts-autotag',
+        -- requires treesitter
+    })
+
 end)
