2d61b91f58b3ad51ebb648273b24d1a5c856ed93
David Attlee <dattlee@bloomberg>
2023-05-08 23:18:05 -0400

    show diagnostic shortcut

--- lua/pseudogram/remap.lua
+++ lua/pseudogram/remap.lua
@@ -71,3 +71,6 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+-- open vim diagnostic window for error under cursor
+vim.keymap.set('n', "<leader>e", function() vim.diagnostic.open_float() end)
