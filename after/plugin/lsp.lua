d800deb4875932e0ac03d01b80d6847e96e5f77c
David Attlee <dattlee@bloomberg>
2023-06-02 18:14:46 -0400

    add lsp saga, yaaas

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -53,9 +53,11 @@ function on_attach(opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+  -- vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set('n', "<leader>ca", "<cmd>Lspsaga code_action<cr>")
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  -- vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("n", "<leader>vrn", "<cmd>Lspsaga rename<cr>")
 
   -- not sure what the following do
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
