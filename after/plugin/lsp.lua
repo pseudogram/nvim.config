6d7b14293e4d1caf02bbcacd8dbc06ac348ed414
David Attlee <pseudogrammer@gmail.com>
2023-06-25 18:15:19 -0400

    formatting

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,11 +3,11 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'tsserver',
-  'rust_analyzer',
-  'pylsp',
-  'clangd',
-  'lua_ls'
+    'tsserver',
+    'rust_analyzer',
+    'pylsp',
+    'clangd',
+    'lua_ls'
 })
 
 -- -- Fix Undefined global 'vim'
@@ -23,19 +23,19 @@ lsp.ensure_installed({
 
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
 })
 
-cmp_mappings['<Tab>'] =  cmp.mapping.confirm({ select = true })
+cmp_mappings['<Tab>'] = cmp.mapping.confirm({ select = true })
 cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+    mapping = cmp_mappings
 })
 
 lsp.set_preferences({
@@ -49,47 +49,44 @@ lsp.set_preferences({
 })
 
 function on_attach(opts)
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-  -- vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set('n', "<leader>ca", "<cmd>Lspsaga code_action<cr>")
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  -- vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("n", "<leader>vrn", "<cmd>Lspsaga rename<cr>")
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    -- vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set('n', "<leader>ca", "<cmd>Lspsaga code_action<cr>")
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    -- vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>vrn", "<cmd>Lspsaga rename<cr>")
 
-  -- not sure what the following do
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    -- not sure what the following do
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
 end
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-  on_attach(opts)
+    local opts = { buffer = bufnr, remap = false }
+    on_attach(opts)
 end)
 
-lsp.skip_server_setup({'tsserver'})
+lsp.skip_server_setup({ 'tsserver' })
 
 lsp.setup()
 
 -- Setup with typescript.nvim -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/guides/quick-recipes.md#setup-with-typescriptnvim
 require("typescript").setup({
-            disable_commands = false, -- prevent the plugin from creating Vim commands
-            debug = false, -- enable debug logging for commands
-            go_to_source_definition = {
-                fallback = true, -- fall back to standard LSP definition on failure
-            },
-            server = { -- pass options to lspconfig's setup method
-                on_attach = on_attach,
-            },
-        })
+    disable_commands = false,         -- prevent the plugin from creating Vim commands
+    debug = false,                    -- enable debug logging for commands
+    go_to_source_definition = {
+        fallback = true,              -- fall back to standard LSP definition on failure
+    },
+    server = {                        -- pass options to lspconfig's setup method
+        on_attach = on_attach,
+    },
+})
 
 
 vim.diagnostic.config({
     virtual_text = true
 })
-
-
-
