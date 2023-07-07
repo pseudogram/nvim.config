4b33bcbf4bd937f49e52af58eb47b87a96ce0436
David Attlee <dattlee@bloomberg>
2023-07-07 14:22:10 -0400

    remove netrw banner

--- lua/pseudogram/set.lua
+++ lua/pseudogram/set.lua
@@ -36,7 +36,7 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_winsize = 75
 
 -- hide netrw banner that gives usage info
--- vim.g.netrw_banner = 0
+vim.g.netrw_banner = 0
 
 -- ignore case letters when searching
 vim.opt.ignorecase = true
