1b4c243b928061540099b8e8b275dc6b1cdd406a
David Attlee <dattlee@bloomberg>
2023-07-07 14:19:09 -0400

    add oil directory editor

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,27 +1,17 @@
-local oil = require("oil")
+-- local oil = require("oil")
+-- Returns `cwd` or path of current buffer depending on whether the current window is terminal or nothing
+local function current_path()
+  local current_win_info = vim.fn["getwininfo"](vim.api.nvim_get_current_win())
+  if current_win_info == nil or #current_win_info == 0 then
+    return vim.fn["getcwd"]()
+  end
+  return vim.fn.expand("%:p:h")
+end
 
-oil.setup({
-    columns = {
-        "icon",
-        -- "permissions"
-    },
-    default_file_explorer = false,
-    use_default_keymaps = false,
-    keymaps = {
-        ["g?"] = "actions.show_help",
-        ["<CR>"] = "actions.select",
-        ["<C-s>"] = "actions.select_split",
-        ["<C-v>"] = "actions.select_vsplit",
-        ["<C-t>"] = "actions.select_tab",
-        ["<Esc>"] = "actions.close",
-        ["<Tab>"] = "actions.preview",
-        ["<C-r>"] = "actions.refresh",
-        ["-"] = "actions.parent",
-        ["g."] = "actions.toggle_hidden",
-    },
-    view_options = {
-        show_hidden = true
-    }
-})
-
-vim.keymap.set("n", "<leader>o", function() oil.open() end, { desc = "Open parent directory" })
+vim.keymap.set("n", "<leader>o", function()
+  if vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(0), "filetype") == "oil" then
+    return require("oil").close()
+  else
+    return require("oil").open_float(current_path())
+  end
+end, { desc = "Toggle Oil filemanager" })
--- lua/pseudogram/packer.lua
+++ lua/pseudogram/packer.lua
@@ -154,9 +154,33 @@ return require('packer').startup(function(use)
 
     -- edit directories and files in netrw like a normal buffer
     use {
-      'stevearc/oil.nvim',
+        'stevearc/oil.nvim',
+        config = function()
+            require('oil').setup({
+                columns = {
+                    "icon",
+                    -- "permissions"
+                },
+                -- use_default_keymaps = false,
+                default_file_explorer = false,
+                -- keymaps = {
+                --     ["g?"] = "actions.show_help",
+                --     ["<CR>"] = "actions.select",
+                --     ["<C-s>"] = "actions.select_split",
+                --     ["<C-v>"] = "actions.select_vsplit",
+                --     ["<C-t>"] = "actions.select_tab",
+                --     ["<Esc>"] = "actions.close",
+                --     ["<Tab>"] = "actions.preview",
+                --     ["<C-r>"] = "actions.refresh",
+                --     ["-"] = "actions.parent",
+                --     ["g."] = "actions.toggle_hidden",
+                -- },
+                -- view_options = {
+                --     show_hidden = true
+                -- }
+            })
+        end
     }
 end)
-
 
 
