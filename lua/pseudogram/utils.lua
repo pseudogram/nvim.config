72b0fce81c60dbeee89addf5f88797e7957cfcaa
dattlee <dattlee@bloomberg.net>
2023-09-25 14:16:08 -0400

    lua yank selection util

--- lua/pseudogram/utils.lua
+++ lua/pseudogram/utils.lua
@@ -7,4 +7,13 @@ function M.checkPath(pathSubstring)
     return cwd:find(pathSubstring,1,true)
 end
 
+function M.get_visual_selection()
+    -- Yank current visual selection into the 'v' register
+    --
+    -- Note that this makes no effort to preserve this register
+    vim.cmd('noau normal! "vy"')
+
+    return vim.fn.getreg('v')
+end
+
 return M
