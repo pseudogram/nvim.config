085890c3ea7d232ad88f890475ac2f818f9d5463
Pseudogram <pseudogrammer@gmail.com>
2024-01-30 13:24:35 -0500

    update treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -12,9 +12,13 @@ require 'nvim-treesitter.configs'.setup {
         "vim",
         "vimdoc",
         "query",
-        "markdown", -- needed for lspsaga
-        "markdown_inline" -- needed for lspsaga
+        "markdown",        -- needed for lspsaga
+        "markdown_inline", -- needed for lspsaga
+        "ruby",
+        "html",
+        "embedded_template"
     },
+    embedded_template = { enable = true },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
@@ -47,3 +51,14 @@ require 'nvim-treesitter.configs'.setup {
         enable = true,
     },
 }
+
+-- local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+-- parser_config.embedded_template = {
+--   install_info = {
+--     url = 'https://github.com/tree-sitter/tree-sitter-embedded-template',
+--     files =  { 'src/parser.c' },
+--     requires_generate_from_grammar  = true,
+--   },
+--   filetype = {'eruby'}
+-- }
+vim.treesitter.language.register('embedded_template', 'eruby')
