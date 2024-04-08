51bf7acee124e4dffe6e2ceaa59d9d119bf6283d
Pseudogram <pseudogrammer@gmail.com>
2024-04-08 01:44:02 -0400

    format using LSP if ther is no default formatter

--- lua/custom/plugins/formatter.lua
+++ lua/custom/plugins/formatter.lua
@@ -15,33 +15,35 @@ return {
             filetype = {
                 -- Formatter configurations for filetype "lua" go here
                 -- and will be executed in order
-                lua = {
-                    -- "formatter.filetypes.lua" defines default configurations for the
-                    -- "lua" filetype
-                    require("formatter.filetypes.lua").stylua,
 
-                    -- You can also define your own configuration
-                    function()
-                        -- Supports conditional formatting
-                        if util.get_current_buffer_file_name() == "special.lua" then
-                            return nil
-                        end
+                -- lua = {
+                --     -- "formatter.filetypes.lua" defines default configurations for the
+                --     -- "lua" filetype
+                --     require("formatter.filetypes.lua").stylua,
 
-                        -- Full specification of configurations is down below and in Vim help
-                        -- files
-                        return {
-                            exe = "stylua",
-                            args = {
-                                "--search-parent-directories",
-                                "--stdin-filepath",
-                                util.escape_path(util.get_current_buffer_file_path()),
-                                "--",
-                                "-",
-                            },
-                            stdin = true,
-                        }
-                    end
-                },
+                --     -- You can also define your own configuration
+                --     function()
+                --         -- Supports conditional formatting
+                --         if util.get_current_buffer_file_name() == "special.lua" then
+                --             return nil
+                --         end
+
+                --         -- Full specification of configurations is down below and in Vim help
+                --         -- files
+                --         return {
+                --             exe = "stylua",
+                --             args = {
+                --                 "--search-parent-directories",
+                --                 "--stdin-filepath",
+                --                 util.escape_path(util.get_current_buffer_file_path()),
+                --                 "--",
+                --                 "-",
+                --             },
+                --             stdin = true,
+                --         }
+                --     end
+                -- },
+
                 javascript = {
                     require("formatter.defaults.prettier"),
                 },
@@ -55,13 +57,33 @@ return {
                     require("formatter.defaults.prettier"),
                 },
 
-
                 -- Use the special "*" filetype for defining formatter configurations on
                 -- any filetype
                 ["*"] = {
                     -- "formatter.filetypes.any" defines default configurations for any
                     -- filetype
-                    require("formatter.filetypes.any").remove_trailing_whitespace
+                    require("formatter.filetypes.any").remove_trailing_whitespace,
+
+                    -- lsp formatter
+                    function()
+                        local formatters = require("formatter.util").get_available_formatters_for_ft(vim.bo.filetype)
+                        -- do nothing if other formatters exist
+                        if #formatters > 0 then
+                            return
+                        end
+
+                        -- check if there are any LSP formatters
+                        local lsp_clients = vim.lsp.buf_get_clients()
+                        for _, client in pairs(lsp_clients) do
+                            -- format with the lsp
+                            if client.server_capabilities.documentFormattingProvider then
+                                vim.lsp.buf.format()
+                                return
+                            end
+                        end
+                        -- Optional: default to prettier
+                        -- return prettier()
+                    end,
                 }
             }
         }
