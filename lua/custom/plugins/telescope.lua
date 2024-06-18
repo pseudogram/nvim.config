46fa0a38126c4fca528a319b0247cc356300a866
David Attlee <pseudogrammer@gmail.com>
2024-06-17 23:14:11 -0400

    show dotfiles in telescope

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -6,25 +6,33 @@ return {
         local telescope = require('telescope')
         local builtin = require('telescope.builtin')
         local utils = require('pseudogram.utils')
+        local telescopeConfig = require("telescope.config")
+
+        -- Clone the default Telescope configuration
+        local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+        -- I want to search in hidden/dot files.
+        table.insert(vimgrep_arguments, "--hidden")
+        -- I don't want to search in the `.git` directory.
+        table.insert(vimgrep_arguments, "--glob")
+        table.insert(vimgrep_arguments, "!**/.git/*")
+
 
         telescope.setup({
             defaults = {
                 path_display = {
                     "smart"
                 },
-                dynamic_preview_title = true
+                dynamic_preview_title = true,
+
+                -- `hidden = true` is not supported in text grep commands.
+                vimgrep_arguments = vimgrep_arguments,
             },
-        })
-
-        -- local builtin = require('telescope.builtin')
-        -- local utils = require('pseudogram.utils')
-
-        telescope.setup({
-            defaults = {
-                path_display = {
-                    "smart"
+            pickers = {
+                find_files = {
+                    -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+                    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
                 },
-                dynamic_preview_title = true
             },
         })
 
