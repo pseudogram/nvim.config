85ae35f9568741e5b12a114a7b14cf432b5db314
David Attlee <pseudogrammer@gmail.com>
2024-06-17 23:39:35 -0400

    setup treesitter defaults

--- lua/custom/plugins/nvim-treesitter.lua
+++ lua/custom/plugins/nvim-treesitter.lua
@@ -2,6 +2,13 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+    config = function()
+        require'nvim-treesitter.configs'.setup {
+            -- A list of parser names, or "all" (the five listed parsers should always be installed)
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python",
+                                "ruby", "html", "css", "javascript", "typescript"},
+        }
+
     end
-    -- build = ':TSUpdate'
 }
