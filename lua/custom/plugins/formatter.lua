8fc35344a77b1ebb14d199aa4019ecc9c9cd04ac
David Attlee <dattlee@bloomberg.net>
2024-06-25 18:09:49 -0400

    formatting

--- lua/custom/plugins/formatter.lua
+++ lua/custom/plugins/formatter.lua
@@ -43,7 +43,6 @@ return {
                 --         }
                 --     end
                 -- },
-
                 eruby = {
                     require("formatter.defaults.htmlbeautifier"),
                 },
