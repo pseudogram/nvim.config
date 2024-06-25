6fe46314103dacbf3819fad3783222a5d80210a2
David Attlee <dattlee@bloomberg.net>
2024-06-24 22:29:26 -0400

    fix autotags

--- lua/custom/plugins/nvim-treesitter.lua
+++ lua/custom/plugins/nvim-treesitter.lua
@@ -3,12 +3,19 @@ return {
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
     end,
+    lazy = false,
     config = function()
-        require'nvim-treesitter.configs'.setup {
+        require 'nvim-treesitter.configs'.setup {
             -- A list of parser names, or "all" (the five listed parsers should always be installed)
             ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python",
-                                "ruby", "html", "css", "javascript", "typescript"},
+                "css", "javascript", "typescript", "ruby", "html", "xml",
+                "embedded_template" },
+            -- autotag = {
+            --     enable = true,
+            -- },
         }
-
-    end
+    end,
+    dependencies = {
+        'windwp/nvim-ts-autotag'
+    },
 }
--- lua/custom/plugins/nvim-ts-autotag.lua
+++ lua/custom/plugins/nvim-ts-autotag.lua
@@ -1,1 +1,14 @@
-return {'windwp/nvim-ts-autotag'}
+return {
+    'windwp/nvim-ts-autotag',
+    config = function()
+        require('nvim-ts-autotag').setup({
+            opts = {
+                enable_close = true,          -- Auto close tags
+                enable_rename = true,         -- Auto rename pairs of tags
+                enable_close_on_slash = true -- Auto close on trailing </
+            },
+        })
+    end,
+    -- lazy = true,
+    -- event = "VeryLazy",
+}
