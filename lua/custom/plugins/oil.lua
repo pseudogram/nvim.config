37d939b6184d12ebecc76231d59140cc154be2d5
David Attlee <dattlee@bloomberg.net>
2024-09-17 11:19:35 +0100

    fix oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -29,20 +29,20 @@ return {
         -- local oil = require("oil")
         -- Returns `cwd` or path of current buffer depending on whether the current window is terminal or nothing
         local function current_path()
-          local current_win_info = vim.fn["getwininfo"](vim.api.nvim_get_current_win())
-          if current_win_info == nil or #current_win_info == 0 then
-            return vim.fn["getcwd"]()
-          end
-          return vim.fn.expand("%:p:h")
+            local current_win_info = vim.fn["getwininfo"](vim.api.nvim_get_current_win())
+            if current_win_info == nil or #current_win_info == 0 then
+                return vim.fn["getcwd"]()
+            end
+            return vim.fn.expand("%:p:h")
         end
 
         vim.keymap.set("n", "<leader>o", function()
-          if vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(0), "filetype") == "oil" then
-            return oil.close()
-          else
-            return oil.open_float(current_path())
-          end
+            if vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(0), "filetype") == "oil" then
+                return oil.close()
+            else
+                return oil.open_float(current_path())
+            end
         end, { desc = "Toggle Oil filemanager" })
+        oil.setup({ default_file_explorer = false })
     end
 }
-
