05f6e2f9b7ed5a9e9dc57aa2864bf3b4682e22ea
David Attlee <dattlee@bloomberg.net>
2024-12-07 19:39:10 -0500

    add docs

--- lua/pseudogram/set.lua
+++ lua/pseudogram/set.lua
@@ -22,6 +22,11 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
+
+-- Reserve a space in the gutter
+-- This will avoid an annoying layout shift in the screen
+vim.opt.signcolumn = 'yes'
+
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
