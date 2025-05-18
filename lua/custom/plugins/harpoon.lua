193d3a0dc90e080cc06a9ea911e1e4cba77d327d
pseudogram <pseudogrammer@gmail.com>
2025-05-18 10:57:30 -0400

    upgrade to haproon 2

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,15 +1,24 @@
 return {
-    'ThePrimeagen/harpoon',
-    config = function()
-        local mark = require("harpoon.mark")
-        local ui = require("harpoon.ui")
+  'ThePrimeagen/harpoon',
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local harpoon = require("harpoon")
 
-        vim.keymap.set("n", "<leader>ah", mark.add_file)
-        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+    -- REQUIRED
+    harpoon:setup()
+    -- REQUIRED
 
-        vim.keymap.set("n", "<C-h><C-h>", function() ui.nav_file(1) end)
-        vim.keymap.set("n", "<C-j><C-j>", function() ui.nav_file(2) end)
-        vim.keymap.set("n", "<C-k><C-k>", function() ui.nav_file(3) end)
-        vim.keymap.set("n", "<C-l><C-l>", function() ui.nav_file(4) end)
-    end
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+    vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+  end
 }
