3d5ce2be4c33ed2f7e08b853a9208b3f0e239e65
pseudogram <pseudogrammer@gmail.com>
2025-05-18 10:57:48 -0400

    fix linting, and exclude file correctly

--- lua/custom/plugins/nvim-lint.lua
+++ lua/custom/plugins/nvim-lint.lua
@@ -1,35 +1,30 @@
 return {
-    'mfussenegger/nvim-lint',
-    config = function()
-        local lint = require("lint")
-        lint.linters.cspell = require("lint.util").wrap(lint.linters.cspell, function(diagnostic)
-            diagnostic.severity = vim.diagnostic.severity.HINT
+  'mfussenegger/nvim-lint',
+  config = function()
+    local lint = require("lint")
+    lint.linters.cspell = require("lint.util").wrap(lint.linters.cspell, function(diagnostic)
+      diagnostic.severity = vim.diagnostic.severity.HINT
 
-            return diagnostic
-        end)
-        lint.linters.codespell = require("lint.util").wrap(lint.linters.codespell, function(diagnostic)
-            diagnostic.severity = vim.diagnostic.severity.HINT
+      return diagnostic
+    end)
+    lint.linters.codespell = require("lint.util").wrap(lint.linters.codespell, function(diagnostic)
+      diagnostic.severity = vim.diagnostic.severity.HINT
 
-            return diagnostic
-        end)
+      return diagnostic
+    end)
 
-        vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, { -- InsertLeave would be good
-            callback = function()
-                -- -- try_lint without arguments runs the linters defined in `linters_by_ft`
-                -- -- for the current filetype
-                -- require("lint").try_lint()
-
-                local dont_lint = {'netrw'}
-                for i=1, #dont_lint do
-                    if vim.bo.ft == dont_lint[i] then
-                        return
-                    end
-                end
-                -- You can call `try_lint` with a linter name or a list of names to always
-                -- run specific linters, independent of the `linters_by_ft` configuration
-                lint.try_lint("cspell")
-                lint.try_lint("codespell")
-            end,
-        })
-    end
+    local excluded_filetype = {
+      netrw = true,
+      harpoon = true
+    }
+    vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
+      callback = function()
+        local ft = vim.bo.filetype
+        if not excluded_filetype[ft] then
+          lint.try_lint("cspell")
+          lint.try_lint("codespell")
+        end
+      end,
+    })
+  end
 }
